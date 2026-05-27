b3ef61271016003fcf63e4ee047268aeab2d9e0d
Pawan <payadav142@gmail.com>
2026-05-27 08:04:48 +0530

    Added sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,28 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://calculator-hub-murex.vercel.app",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://calculator-hub-murex.vercel.app/brick-calculator",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://calculator-hub-murex.vercel.app/cement-calculator",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://calculator-hub-murex.vercel.app/concrete-calculator",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://calculator-hub-murex.vercel.app/paint-calculator",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://calculator-hub-murex.vercel.app/tile-calculator",
+      lastModified: new Date(),
+    },
+  ];
+}
