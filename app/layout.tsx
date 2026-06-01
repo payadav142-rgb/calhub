d836d23314105eabf2d828dbc268572c10925f55
Pawan <payadav142@gmail.com>
2026-06-01 10:23:20 +0530

    fix adsense layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -69,8 +69,8 @@ export default function RootLayout({
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9673900751625361"
           strategy="afterInteractive"
-          crossOrigin="anonymous"
-        />
+          crossOrigin="anonymous">
+        </Script>
 
         {/* STRUCTURED DATA */}
         <script
